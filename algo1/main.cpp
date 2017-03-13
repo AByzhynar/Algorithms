8219491985d049747dfd380ec294d730b7ee8218
Andrew Byzhynar <abyzhynar@luxoft.com>
2017-03-13 16:32:43 +0200

    First algorithm

---
+++ algo1/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stddef.h>
+#include <vector>
+
+
+using namespace std;
+
+int SearchElement(vector<int>& array, const int key) {
+    int i = 0;
+    array.push_back(key);
+    while(array[i] != key) {
+        ++i;
+    }
+    array.pop_back();
+    if(i != array.size()) {
+        return i;
+    }
+    return (-1);
+}
+
+
+template <class TFunc, class TResult, class TParam1, class TParam2>
+void test(TResult expected ,TFunc f, TParam1 parameters, TParam2 value) {
+    auto got = f(parameters);
+    if (got != expected) {
+        cerr << "failed" << endl;
+        cerr << "Expected: " << expected << endl;
+        cerr << "Actual: " << got << endl;
+    }
+
+}
+
+
+template <class TFunc, class TResult, class TParam1>
+void test(TResult expected ,TFunc f, TParam1 parameters) {
+    auto got = f(parameters);
+    if (got != expected) {
+        cerr << "failed" << endl;
+        cerr << "Expected: " << expected << endl;
+        cerr << "Actual: " << got << endl;
+    }
+
+}
+
+void test_search() {
+typedef vector<int> Array;
+    test(8, SearchElement, Array({0, 1, 2, 3, 4, 5, 6, 7, 8}), 8);
+}
+
+int main() {
+    test_search();
+    return 0;
+}
+
+
+
