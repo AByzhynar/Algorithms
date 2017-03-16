42608940cce1c7f071cca04e06448d0e927aefc7
Andrew Byzhynar <abyzhynar@luxoft.com>
2017-03-16 16:51:40 +0200

    Add tests

--- algo1/main.cpp
+++ algo1/main.cpp
@@ -15,8 +15,8 @@ ostream& operator<<(ostream& o, const vector<T>& v) {
 }
 
 
-size_t SearchElement(vector<int>& array, const int key) {
-    size_t i = 0;
+int SearchElement(vector<int>& array, const int key) {
+    int i = 0;
     array.push_back(key);
     while(array[i] != key) {
         ++i;
@@ -55,9 +55,35 @@ void test(TResult expected ,TFunc f, TParam1 p1) {
 }
 
 void test_search() {
-typedef vector<int> Array;
-const size_t expected_index = 6;
-    test(expected_index, SearchElement, Array({0, 1, 2, 3, 4, 5, 6, 7, 8}), 8);
+  typedef vector<int> Array;
+  const int expected_index = 8;
+  auto search = SearchElement;
+  // Key does not exist
+
+  test(-1, search, Array(), expected_index); // Degerate (empty array)
+  test(-1, search, Array({expected_index - 1}), expected_index); // Trivial1  1 element array
+  test(-1, search, Array({expected_index - 1, expected_index + 1}), expected_index); // Trivial2 (2 elements array)
+
+  // General
+  test(-1, search, Array({1, 2, 3, 4, 5, 6, 7}), expected_index);
+  test(-1, search, Array({9, 10, 11, 12}), expected_index);
+  test(-1, search, Array({4, 1, 2, 7, 10}), expected_index);
+
+  // Key exists
+  // Degerate (empty array)
+  // Not applicable
+
+  test(0, search, Array({expected_index}), expected_index); // Trivial1  1 element array
+  test(0, search, Array({expected_index, expected_index + 1}), expected_index); // Trivial2 (2 elements array)
+  test(1, search, Array({expected_index - 1, expected_index}), expected_index); // Trivial2 (2 elements array)
+  test(8, search, Array({0, 1, 2, 3, 4, 5, 6, 7, expected_index}), expected_index); // General
+  test(0, search, Array({expected_index, 9, 10, 11, 12}), expected_index); // General
+  test(2, search, Array({4, 1, expected_index, 7, 10}), expected_index);  // General
+
+
+  test(0, search, Array({expected_index, 1, expected_index, 7, 10}), expected_index); // General
+  test(2, search, Array({2, 1, expected_index, 7, expected_index}), expected_index); // General
+
 }
 
 int main() {
